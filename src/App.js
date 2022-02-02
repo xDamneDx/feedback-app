1a6ef7f2268914617758a47324b27f0637807009
Владимир Ливинский <xDamneDx@Gmail.com>
2022-02-02 19:05:30 +0200

    'Forms, Validation & Simple Animation' completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
 import Header from "./components/Header";
 import feedbackData from "./data/feedbackData";
+import FeedbackForm from "./components/FeedbackForm";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 
 function App() {
   const [feedback, setFeedback] = useState(feedbackData);
-  const feedbackDelete = (id) => {
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
+  const deleteFeedback = (id) => {
     if (window.confirm("Delete? Are you sure?")) {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
@@ -16,8 +22,9 @@ function App() {
     <>
       <Header />
       <div className="container">
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={feedbackDelete} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import { motion, AnimatePresence } from "framer-motion";
 import React from "react";
 import FeedbackItem from "./FeedbackItem";
 
@@ -8,13 +9,32 @@ function FeedbackList({ feedback, handleDelete }) {
 
   return (
     <ul>
-      {feedback.map((item) => (
-        <li key={item.id}>
-          <FeedbackItem item={item} handleDelete={handleDelete} />
-        </li>
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <li key={item.id}>
+              <FeedbackItem item={item} handleDelete={handleDelete} />
+            </li>
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </ul>
   );
+
+  // return (
+  //   <ul>
+  //     {feedback.map((item) => (
+  //       <li key={item.id}>
+  //         <FeedbackItem item={item} handleDelete={handleDelete} />
+  //       </li>
+  //     ))}
+  //   </ul>
+  // );
 }
 
 export default FeedbackList;
